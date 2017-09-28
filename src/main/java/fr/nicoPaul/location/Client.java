d7c982da525b6defd76c3e1d26e6b87e685c93a0
paul <paul604@users.noreply.github.com>
2017-09-28 14:30:17 +0200

    Client.java fin

--- src/main/java/fr/nicoPaul/location/Client.java
+++ src/main/java/fr/nicoPaul/location/Client.java
@@ -1,7 +1,10 @@
 package fr.nicoPaul.location;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by Paul on 18/09/17.
+ *
  */
 public class Client {
 
@@ -10,6 +13,11 @@ public class Client {
     private String adresse;
     private String nmeroTel;
     private String email;
+    private List<Location> locations;
+
+    //-----------------------------
+    //        Constructeur
+    //-----------------------------
 
     public Client(String nom, String prenom, String adresse, String nmeroTel, String email) {
         this.nom = nom;
@@ -17,10 +25,62 @@ public class Client {
         this.adresse = adresse;
         this.nmeroTel = nmeroTel;
         this.email = email;
+        this.locations = new ArrayList<>();
     }
 
     public Client(String nom, String prenom) {
+        this(nom, prenom, null, null, null);
+    }
+
+    //-----------------------------
+    //          Get & Set
+    //-----------------------------
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
         this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
         this.prenom = prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getNmeroTel() {
+        return nmeroTel;
+    }
+
+    public void setNmeroTel(String nmeroTel) {
+        this.nmeroTel = nmeroTel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Location> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(List<Location> locations) {
+        this.locations = locations;
     }
 }
