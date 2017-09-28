754869b0267164beed9f1dc47e1301928dc5bdb4
Nicolas Bourges <nicolas.bourges@etu.univ-nantes.fr>
2017-09-28 14:37:48 +0200

    Mise à jour classes TabLit, SouleveMalades, FauteuilRoulant, Dimension, Article

--- src/main/java/fr/nicoPaul/stocks/Article.java
+++ src/main/java/fr/nicoPaul/stocks/Article.java
@@ -4,4 +4,69 @@ package fr.nicoPaul.stocks;
  * Created by Paul on 18/09/17.
  */
 public abstract class Article {
+
+    private String reference;
+    private String marque;
+    private String modele;
+    private double prix;
+    private int nbDispo;
+
+    //-------------------------------
+    //   Constructeur de la classe
+    //-------------------------------
+
+
+    public Article(String reference, String marque, String modele, double prix, int nbDispo) {
+        this.reference = reference;
+        this.marque = marque;
+        this.modele = modele;
+        this.prix = prix;
+        this.nbDispo = nbDispo;
+    }
+
+    //-------------------------------
+    //      Getters et Setters
+    //         de la classe
+    //-------------------------------
+
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public String getModele() {
+        return modele;
+    }
+
+    public void setModele(String modele) {
+        this.modele = modele;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public int getNbDispo() {
+        return nbDispo;
+    }
+
+    public void setNbDispo(int nbDispo) {
+        this.nbDispo = nbDispo;
+    }
 }
--- src/main/java/fr/nicoPaul/stocks/SouleveMalade.java
+++ src/main/java/fr/nicoPaul/stocks/SouleveMalade.java
@@ -4,4 +4,32 @@ package fr.nicoPaul.stocks;
  * Created by Paul on 18/09/17.
  */
 public class SouleveMalade extends Article {
+
+    private int capLevage;
+    private int degrePivo;
+
+    //-------------------------------
+    //   Constructeur de la classe
+    //-------------------------------
+
+
+    public SouleveMalade(String reference, String marque, String modele, double prix, int nbDispo, int capLevage, int degrePivo) {
+        super(reference, marque, modele, prix, nbDispo);
+        this.capLevage = capLevage;
+        this.degrePivo = degrePivo;
+    }
+
+    //-------------------------------
+    //            Getters
+    //         de la classe
+    //-------------------------------
+
+
+    public int getCapLevage() {
+        return capLevage;
+    }
+
+    public int getDegrePivo() {
+        return degrePivo;
+    }
 }
